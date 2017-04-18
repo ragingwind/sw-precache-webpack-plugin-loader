06ffb129204718f8639f3808ea66416c4f9525cb
ragingwind <ragingwind@gmail.com>
2017-04-18 13:39:15 +0900

    fix: Update name of the plugin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-const SWPrecacheWebpackDevServerPlugin = require('sw-precache-webpack-plugin');
+const SWPrecacheWebpackDevServerPlugin = require('sw-precache-webpack-dev-server-plugin');
 const isWebpackDevServer = require('is-webpack-dev-server');
 
 module.exports = isWebpackDevServer ? SWPrecacheWebpackDevServerPlugin : SWPrecacheWebpackPlugin;
